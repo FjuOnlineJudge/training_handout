932677994a2c8773cb0fbda3e6646de72b6cdd38
allem40306 <allem40306@gmail.com>
2019-01-12 21:27:23 +0800

    revise PQ.cpp

--- BasicDS/PQ.cpp
+++ BasicDS/PQ.cpp
@@ -5,17 +5,11 @@ using namespace std;
 int main(){
     priority_queue<int>Q;
     Q.push(2);
-    cout<<Q.top()<<'\n';
+    cout<<Q.top()<<'\n';// 2
     Q.push(5);
-    cout<<Q.top()<<'\n';
+    cout<<Q.top()<<'\n';// 5
     Q.pop();
-    cout<<Q.top()<<'\n';
+    cout<<Q.top()<<'\n';// 2
     Q.push(3);
-    cout<<Q.top()<<'\n';
+    cout<<Q.top()<<'\n';// 3
 }
-/*
-2
-5
-2
-3
-*/
