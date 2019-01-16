e0ea178e01fc34ca245a4ff047dfcf9edb181b6b
allem40306 <allem40306@gmail.com>
2019-01-16 22:40:48 +0800

    add Grraph code

---
+++ Graph/Kruskal.cpp
@@ -0,0 +1,18 @@
+struct Edge{
+    int s,t,w;
+    bool operaotr<(const Edge&rhs)const{
+        return w<rhs.w;
+    }
+};
+void Kruskal(){
+    int cost=0;
+    vector<Edge>E;
+    init();
+    for(auto it:E){
+        it.s=Find(it.s);
+        it.t=Find(it.t);
+        if(it.s==it.t)continue;
+        cost+=it.w;
+        Union(it.s,it.t);
+    }
+}
